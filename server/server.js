b12604e19e611dd4fc7d09cd3a46f990d144a96f
Amanda <albullington@gmail.com>
2018-04-23 17:36:24 -0700

    description module rendering fully, pictures rendering partially

--- server/server.js
+++ server/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const path = require('path');
+const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
 const port = 4567;
 
 app.use(cors());
-app.use(express.static('/client/src'));
+app.use(morgan('dev'));
+app.use(express.static('client'));
 
-app.get('/', (req, res, next) => {
-  res.json({msg: 'This is CORS-enabled for all origins!'});
+app.listen(port, () => {
+  console.log(`CORS-enabled web server listening on ${port}`);
 });
-
-app.listen(port, () => console.log(`CORS-enabled web server listening on ${port}`));
